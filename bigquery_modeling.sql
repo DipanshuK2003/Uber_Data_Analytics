c3893e80cd266a1be67800ce3a689b65fe5a8d20
DipanshuK2003 <104434675+DipanshuK2003@users.noreply.github.com>
2025-11-18 19:10:59 +0530

    Add files via upload

---
+++ bigquery_modeling.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE TABLE `my-test-prjkt.uber_data_engineering.tbl_analytics` AS
+SELECT
+  f.VendorID,
+  d.tpep_pickup_datetime,
+  d.tpep_dropoff_datetime,
+  p.passenger_count,
+  t.trip_distance,
+  r.rate_code_name,
+  pick.pickup_latitude,
+  pick.pickup_longitude,
+  drop.dropoff_latitude,
+  drop.dropoff_longitude,
+  pay.payment_type_name,
+  f.fare_amount,
+  f.extra,
+  f.mta_tax,
+  f.tip_amount,
+  f.tolls_amount,
+  f.improvement_surcharge,
+FROM `my-test-prjkt.uber_data_engineering.fact_table` f
+JOIN `my-test-prjkt.uber_data_engineering.datetime_dim` d ON f.datetime_id = d.datetime_id
+JOIN `my-test-prjkt.uber_data_engineering.passenger_count_dim` p ON f.passenger_count_id = p.passenger_count_id
+JOIN `my-test-prjkt.uber_data_engineering.trip_distance_dim` t ON f.trip_distance_id = t.trip_distance_id
+JOIN `my-test-prjkt.uber_data_engineering.rate_code_dim` r ON f.rate_code_id = r.rate_code_id
+JOIN `my-test-prjkt.uber_data_engineering.pickup_location_dim` pick ON f.pickup_location_id = pick.pickup_location_id
+JOIN `my-test-prjkt.uber_data_engineering.dropoff_location_dim` drop ON f.dropoff_location_id = drop.dropoff_location_id
+JOIN `my-test-prjkt.uber_data_engineering.payment_type_dim` pay ON f.payment_type_id = pay.payment_type_id;
